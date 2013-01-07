887e81e6a8ec517527be16c0f174d43eebc7b8ea
zxl777 <sky@itoytoy.com>
2013-01-07 23:55:21 +0800

    实现了两个球，和设置了初速度。

---
+++ Assets/Ball.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		rigidbody.velocity = new Vector3(1,0,0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Assets/Touch.cs
+++ Assets/Touch.cs
@@ -14,10 +14,18 @@ public class Touch : MonoBehaviour
 	void Start ()
 	{
 		Line = gameObject.GetComponent<LineRenderer>();
+		Instantiate(ball,new Vector3(-4,8,0), transform.rotation);
 	}
 	
 void Update() 
 {
+	GameObject table = GameObject.Find("Table");
+	GameObject ball1 = GameObject.Find("Ball1");
+	if (ball1.transform.position.y < table.transform.position.y)
+	{
+		ball1.transform.position = new Vector3(-2,10,0);
+		ball1.rigidbody.velocity = new Vector3(1,0,0);
+	}
 	for (int i = 0; i < Input.touchCount; i++) 
 	{
         //if (Input.GetTouch(i).phase == TouchPhase.Began) 
@@ -38,8 +46,9 @@ void Update()
              	
 				Debug.Log("touch = "+touchPosition.x + "," + touchPosition.y +"," + camera.nearClipPlane + "   ,(" + p.x + ","+p.y+")");
 				
-				Line.SetPosition(1,new Vector2(p.x,p.y));
+				//Line.SetPosition(1,new Vector2(p.x,p.y));
 					
+				
        		 	//ball.transform.position = p;
        		 	//double the size to create a larger target to tap on
        		 	//ball.transform.localScale = new Vector3(1,1,1);
